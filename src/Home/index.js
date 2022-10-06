50710fbb5e4f398c8ab7cf833a7120f30735cfcd
MarcusSoaresCarioca <111324245+MarcusSoaresCarioca@users.noreply.github.com>
2022-10-06 14:51:36 -0300

    styles app

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,22 +1,72 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity } from 'react-native';
 import styles from './style';
+import { AntDesign } from '@expo/vector-icons';
 
 
 export default function Home() {
+
+  const [produtos, setProdutos] = useState(null);
+  const [lista, setLista] = useState([]);
+
+  function adicionar() {
+    if (produtos !== null && produtos !== '') {
+      let numeroAl = 50 * Math.random(2)
+      setLista((arr) => [...arr, {
+        id: numeroAl,
+        valor: produtos,
+      }])
+      setProdutos(null);
+    }
+
+    else {
+      alert('Preencha o campo')
+    }
+  }
+
   return (
     <View style={styles.container}>
-        <View style={styles.divHeader}>
-            <Image style={styles.divImg} source={require("../assets/img/img.png")}></Image>
+      <StatusBar />
+      <View style={styles.divHeader}>
+        <Image style={styles.divImg} source={require("../assets/img/img.png")}></Image>
+      </View>
+
+      <View style={styles.divInputs}>
+        <View style={styles.divInput}>
+          <TextInput style={styles.input}
+            placeholder='Produtos'
+            value={produtos}
+            onChangeText={setProdutos}
+            keyboardType='default'>
+
+          </TextInput>
         </View>
 
-        <View style={styles.divInputs}>
-
+        <View style={styles.divBtn}>
+          <TouchableOpacity style={styles.btn} onPress={adicionar}>
+            <AntDesign name="plus" size={27} color="#fff" />
+          </TouchableOpacity>
         </View>
+      </View>
 
-        <View style={styles.divLista}>
+      <View style={styles.divLista}>
+        <FlatList
+          style={{ width: '100%' }}
+          data={lista}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => {
+            return (
+              <View style={item.tipo === 1 ? styles.itemListaReceita : styles.itemListaDespesa}>
+                <Text style={styles.txtTipo}>{item.tipo === 1 ? "Receita" : "Despesas"}</Text>
+                <Text style={styles.valorItem}> {item.tipo === 1 ? "R$" : "R$-"}{item.valor}</Text>
+              </View>
+            )
+          }}>
 
-        </View>
+
+        </FlatList>
+      </View>
     </View>
   );
 }
--- src/Home/style.js
+++ src/Home/style.js
@@ -4,26 +4,62 @@ const styles = StyleSheet.create({
 
     container:{
         flex:1,
+        backgroundColor:'#fff'
     },
 
     divHeader:{
         flex:4,
-        backgroundColor:'red'
+        alignItems:'center',
+        justifyContent:'center'
     },
 
     divImg:{
-        width:60,
-        height:60
+        width:170,
+        height:170
     },
 
     divInputs:{
-        flex:2,
-        backgroundColor:'blue'
+        flex:1,
+        flexDirection:'row',
+    },
+
+    divInput:{
+        flex:1,
+        backgroundColor:'#0107FF',
+        alignItems:'center',
+        justifyContent:'center'
+    },
+
+    input:{
+        backgroundColor:'#fff',
+        width:150,
+        height:50,
+        borderRadius:20,
+        
+    },
+
+    divBtn:{
+        flex:0.5,
+        backgroundColor:'#0107FF',
+        alignItems:'center',
+        justifyContent:'center'
+    },
+
+    btn:{
+        width:50,
+        height:50,
+        borderRadius:20,
+        backgroundColor:'#5198FF',
+        alignItems:'center',
+        justifyContent:'center'
     },
 
     divLista:{
-        flex:6
-    }
+        flex:6,
+        backgroundColor:'#0107FF',
+        borderTopColor:'#fff',
+        borderTopWidth:5
+    },
 
 })
 
